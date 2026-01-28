dbbf98041f3f5f72d800cf0fd8d95716f321fd84
Monsieur-Code1 <abdelrhmanelsyed7@gmail.com>
2026-01-28 21:19:05 +0300

    edit

--- pages/products/products.js
+++ pages/products/products.js
@@ -17,7 +17,7 @@ function renderProducts() {
   const showProducts = products.products
     .map((product) => {
       return `
-   <div class="card m-1" style="width: 250px; height: 450px;"
+   <div class="card m-1 border-2" style="width: 250px; height: 450px;"
 >
      
       <img 
@@ -31,16 +31,16 @@ function renderProducts() {
 
       <div class="card-body d-flex flex-column justify-content-between ">
       
-      <h5 class="card-title fs-6" style="font-size:25px">${product.title}</h5>
+      <h5 class="card-title fs-6 text-danger" style="font-size:25px">${product.title}</h5>
       
         <p class="card-text" style="font-size:18px">
          ${product.description.slice(0, 50) + '...'}
         </p>
-        <div  class="d-flex justify-content-between">
+        <div  class="d-flex text-primary  justify-content-between">
         <span>${product.price}</span>
         <span>${product.rating}⭐</span>
         </div>
-        <a href="#" class="btn btn-primary">ORDER NOW</a>
+        <a href="#" class="btn btn-warning">ORDER NOW</a>
       </div>
     </div>`;
     })
